7f896751c7f375d2a29f3ae776e0a7e379e2976d
Scott M Stark <starksm64@gmail.com>
2024-04-01 10:19:37 -0600

    Use and ApplicationScoped EMF as the Weld setPersistenceUnitFactory
    Signed-off-by: Scott M Stark <starksm64@gmail.com>

--- runner/src/test/java/org/hibernate/ee/jakarta/data/tck/StandaloneEntityTests.java
+++ runner/src/test/java/org/hibernate/ee/jakarta/data/tck/StandaloneEntityTests.java
@@ -1,31 +1,23 @@
 package org.hibernate.ee.jakarta.data.tck;
 
 import ee.jakarta.tck.data.core.cdi.provider.DirectoryBuildCompatibleExtension;
-import ee.jakarta.tck.data.framework.read.only.AsciiCharacters;
 import ee.jakarta.tck.data.framework.read.only.AsciiCharacters_;
-import ee.jakarta.tck.data.framework.read.only.CustomRepository;
 import ee.jakarta.tck.data.framework.read.only.CustomRepository_;
-import ee.jakarta.tck.data.framework.read.only.NaturalNumbers;
 import ee.jakarta.tck.data.framework.read.only.NaturalNumbers_;
-import ee.jakarta.tck.data.framework.read.only.PositiveIntegers;
 import ee.jakarta.tck.data.framework.read.only.PositiveIntegers_;
 import ee.jakarta.tck.data.standalone.entity.Boxes_;
 import ee.jakarta.tck.data.standalone.entity.MultipleEntityRepo_;
 import ee.jakarta.tck.data.standalone.entity.EntityTests;
-import jakarta.data.repository.Repository;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.CDI;
 import jakarta.enterprise.inject.spi.InjectionPoint;
 import jakarta.inject.Inject;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import org.hibernate.Hibernate;
 import org.jboss.weld.junit5.WeldInitiator;
 import org.jboss.weld.junit5.WeldJunit5Extension;
 import org.jboss.weld.junit5.WeldSetup;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.HashMap;
@@ -33,6 +25,9 @@ import java.util.function.Function;
 
 @ExtendWith(WeldJunit5Extension.class)
 public class StandaloneEntityTests extends EntityTests {
+    @Inject
+    EntityManagerFactory appEMF;
+
     static class EMFAction implements Function<InjectionPoint, EntityManagerFactory> {
         @Override
         public EntityManagerFactory apply(InjectionPoint injectionPoint) {
@@ -52,9 +47,11 @@ public class StandaloneEntityTests extends EntityTests {
                     PositiveIntegers_.class,
                     CustomRepository_.class,
                     MultipleEntityRepo_.class,
+                    EntityManagerFactoryProducer.class,
                     DirectoryBuildCompatibleExtension.class)
             .activate(RequestScoped.class)
-            .setPersistenceUnitFactory((Function) new EMFAction())
+            .inject(this)
+            .setPersistenceUnitFactory((ip) -> appEMF)
             .build()
             ;
 
